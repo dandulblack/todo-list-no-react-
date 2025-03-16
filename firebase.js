6500ce1887af8637c099a07b907d677df859bd73
Daniel Černý <dandul.black@gmail.com>
2025-03-16 20:52:06 +0100

    added firebase inicaliation

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+// Konfigurace Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCme57MnVJhMFXEecMF2auZotEIZXwazMw",
+    authDomain: "todo-list-8e82f.firebaseapp.com",
+    projectId: "todo-list-8e82f",
+    storageBucket: "todo-list-8e82f.firebasestorage.app",
+    messagingSenderId: "1069537627232",
+    appId: "1:1069537627232:web:ae8182ddee99a7904a4705"
+};
+
+// Inicializace Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
